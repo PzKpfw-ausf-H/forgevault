c85a70bd25c8f0c1ccc393a9835c85a6a5d33f3f
PzKpfw-ausf-H <nikitanovozhilovvv@gmail.com>
2026-09-09 09:52:33 +0300

    made the domain asset migration. Types file is temporary and only exists until the project migration is done

--- internal/domain/asset.go
+++ internal/domain/asset.go
@@ -2,13 +2,37 @@ package domain
 
 import "time"
 
+type AssetType string
+
+const (
+	AssetType3D    AssetType = "3d"
+	AssetType2D    AssetType = "2d"
+	AssetTypeAudio AssetType = "audio"
+	AssetTypeVFX   AssetType = "vfx"
+	AssetTypeDoc   AssetType = "doc"
+)
+
+func (t AssetType) Valid() bool {
+	switch t {
+	case AssetType2D, AssetType3D, AssetTypeAudio, AssetTypeVFX, AssetTypeDoc:
+		return true
+	default:
+		return false
+	}
+}
+
 type Asset struct {
 	ID          AssetID
 	Title       string
 	Description string
-	Type        AssetType
-	Tags        []string
-	AuthorID    UserID
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
+
+	AuthorName string
+	UploadedBy UserID
+
+	Type             AssetType
+	Tags             []string
+	ProcessingStatus ProcessingStatus
+
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
--- internal/domain/types.go
+++ internal/domain/types.go
@@ -1,21 +1,5 @@
 package domain
 
 type FileID string
-type AssetType string
 
-const (
-	AssetType3D    AssetType = "3d"
-	AssetType2D    AssetType = "2d"
-	AssetTypeAudio AssetType = "audio"
-	AssetTypeVFX   AssetType = "vfx"
-	AssetTypeDoc   AssetType = "doc"
-)
-
-func IsValidAssetType(t AssetType) bool {
-	switch t {
-	case AssetType2D, AssetType3D, AssetTypeAudio, AssetTypeVFX, AssetTypeDoc:
-		return true
-	default:
-		return false
-	}
-}
+// This file exists only until the project migration is done
